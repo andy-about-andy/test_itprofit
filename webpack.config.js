59276925431e8adebfa6289b6268cca0dfd0c3b1
andrey_lutsevich <andrey-360.90@mail.ru>
2023-10-27 21:42:49 +0300

    feat: добавляет форму и кнопку модалки

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.s[ac]ss$/,
+        test: /\.s[ac]ss$/i,
         use: [
           MiniCssExtractPlugin.loader,
           "css-loader",
